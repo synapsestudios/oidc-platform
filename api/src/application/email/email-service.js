4cf457d811e2f393596e76211d6e661f87cabf82
Chris Shiplet <chris@nearengine.com>
2017-02-13 10:47:08 -0700

    Remove unused reference

--- api/src/application/email/email-service.js
+++ api/src/application/email/email-service.js
@@ -3,7 +3,7 @@ const mailgun = require('./drivers/mailgun');
 
 const drivers = { mailgun };
 
-module.exports = (bookshelf) => {
+module.exports = () => {
   if (!drivers[config('/email')]) {
     throw 'Invalid email driver, please set OIDC_EMAIL_DRIVER';
   }
@@ -11,4 +11,3 @@ module.exports = (bookshelf) => {
 };
 
 module.exports['@singleton'] = true;
-module.exports['@require'] = ['bookshelf'];
