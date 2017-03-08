03177d8b0c38785986cbc441ad07ff6749ea164c
Zachary Chavez <zach@synapsestudios.com>
2017-03-07 17:06:07 -0700

    Add resend-invite endpoint

--- api/src/application/api/api-routes.js
+++ api/src/application/api/api-routes.js
@@ -1,6 +1,8 @@
 const Joi = require('joi');
 
-module.exports = (userService, mixedValidation, rowNotExists) => [
+const hoursTillExpirationSchema = Joi.number().integer().greater(0).default(48);
+
+module.exports = (userService, mixedValidation, rowNotExists, rowExists) => [
   {
     method: 'POST',
     path: '/api/invite',
@@ -19,12 +21,46 @@ module.exports = (userService, mixedValidation, rowNotExists) => [
             email: Joi.string().email().required(),
             app_metadata: Joi.object(),
             profile: Joi.object(),
+            hours_till_expiration: hoursTillExpirationSchema,
           },
           {
             email: rowNotExists('user', 'email', 'Email already in use')
           }
         )
       }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/api/resend-invite/{userId}',
+    handler: (request, reply) => {
+      reply(
+        userService.resendUserInvite(
+          request.params.userId,
+          request.payload.app_name,
+          request.payload.hours_till_expiration
+        )
+      );
+    },
+    config: {
+      auth: {
+        strategy: 'client_credentials',
+        scope: 'admin'
+      },
+      validate: {
+        params: mixedValidation(
+          {
+            userId: Joi.any().required(),
+          },
+          {
+            userId: rowExists('user', 'id', 'User not found')
+          }
+        ),
+        payload: {
+          app_name: Joi.string().required(),
+          hours_till_expiration: hoursTillExpirationSchema,
+        }
+      },
     }
   },
   {
@@ -52,4 +88,5 @@ module.exports['@require'] = [
   'user/user-service',
   'validator/mixed-validation',
   'validator/constraints/row-not-exists',
+  'validator/constraints/row-exists',
 ];
