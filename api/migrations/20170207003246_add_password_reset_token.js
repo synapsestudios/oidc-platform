c74587319252e02b315dc671da710211f2584c86
spruce-bruce <aaron@synapsestudios.com>
2017-03-13 10:32:18 -0700

    convert migrations to work in RDS mysql

--- api/migrations/20170207003246_add_password_reset_token.js
+++ api/migrations/20170207003246_add_password_reset_token.js
@@ -1,19 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('SIP_user_password_reset_token', function(table) {
-    table.string('user_id').references('SIP_user.id');
-    table.string('token');
-    table.datetime('expires_at');
-    table.primary(['user_id', 'token']);
-  });
 
-// CREATE TABLE `SIP_user_password_reset_token` (
-//   `user_id` varchar(255) NOT NULL,
-//   `token` varchar(255) NOT NULL,
-//   `expires_at` datetime DEFAULT NULL,
-//   PRIMARY KEY (`user_id`,`token`),
-//   CONSTRAINT `sip_user_password_reset_token_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `SIP_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-// )
+  if (process.env.OIDC_DB_ADAPTER === 'mysql') {
+    return knex.raw(`
+      CREATE TABLE SIP_user_password_reset_token (
+        user_id varchar(255) NOT NULL,
+        token varchar(255) NOT NULL,
+        expires_at datetime DEFAULT NULL,
+        PRIMARY KEY (user_id,token),
+        CONSTRAINT sip_user_password_reset_token_user_id_foreign FOREIGN KEY (user_id) REFERENCES SIP_user (id) ON DELETE CASCADE ON UPDATE CASCADE
+      )
+    `);
+  } else {
+    return knex.schema.createTable('SIP_user_password_reset_token', function(table) {
+      table.string('user_id').references('SIP_user.id');
+      table.string('token');
+      table.datetime('expires_at');
+      table.primary(['user_id', 'token']);
+    });
+  }
 };
 
 exports.down = function(knex, Promise) {
