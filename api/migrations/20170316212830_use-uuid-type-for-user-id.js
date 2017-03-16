de418baea166de740ec965f5b3f35fc5e78c6b1c
Zachary Chavez <zach@synapsestudios.com>
2017-03-16 15:17:42 -0700

    Don't even bother with uuid user ID migration if using mysql

--- api/migrations/20170316212830_use-uuid-type-for-user-id.js
+++ api/migrations/20170316212830_use-uuid-type-for-user-id.js
@@ -1,5 +1,9 @@
 
 exports.up = function(knex, Promise) {
+  if (process.env.OIDC_DB_ADAPTER === 'mysql') {
+    return Promise.resolve();
+  }
+
   return knex.schema.alterTable('SIP_user_password_reset_token', table => {
     table.dropPrimary();
     table.dropForeign('user_id');
@@ -24,6 +28,10 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
+  if (process.env.OIDC_DB_ADAPTER === 'mysql') {
+    return Promise.resolve();
+  }
+
   return knex.schema.alterTable('SIP_user_password_reset_token', table => {
     table.dropPrimary();
     table.dropForeign('user_id');
