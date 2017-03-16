986952088b04b704432ea8584206a639ea3f45ba
Zachary Chavez <zach@synapsestudios.com>
2017-03-16 15:16:27 -0700

    Add migration to switch to uuid type for user ID

---
+++ api/migrations/20170316212830_use-uuid-type-for-user-id.js
@@ -0,0 +1,48 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+    table.dropPrimary();
+    table.dropForeign('user_id');
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+      table.uuid('user_id').notNull().alter();
+      table.primary(['user_id', 'token']);
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user', table => {
+      table.dropPrimary();
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user', table => {
+      table.uuid('id').notNull().alter().primary();
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+      table.foreign('user_id').references('id').inTable('SIP_user').onDelete('CASCADE').onUpdate('CASCADE');
+    });
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+    table.dropPrimary();
+    table.dropForeign('user_id');
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+      table.string('user_id', 36).notNull().alter();
+      table.primary(['user_id', 'token']);
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user', table => {
+      table.dropPrimary();
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user', table => {
+      table.string('id', 36).notNull().alter().primary();
+    });
+  }).then(() => {
+    return knex.schema.alterTable('SIP_user_password_reset_token', table => {
+      table.foreign('user_id').references('id').inTable('SIP_user').onDelete('CASCADE').onUpdate('CASCADE');
+    });
+  });
+};
