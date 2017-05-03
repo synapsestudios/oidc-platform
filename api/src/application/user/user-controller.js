dd07fb54753d8543a8c590c699acc056fecfba9b
Michael Sinnes <michael.sinnes@gmail.com>
2017-05-03 15:43:06 -0700

    will filter the cancel button on the set password page

--- api/src/application/user/user-controller.js
+++ api/src/application/user/user-controller.js
@@ -110,7 +110,7 @@ module.exports = (
       return (request, reply, source, error) => {
         reply.view('reset-password', {
           title: title,
-          returnTo: `${request.query.redirect_uri}?status=cancelled`,
+          returnTo: (title === 'Set Password') ? null : `${request.query.redirect_uri}?status=cancelled`,
           error: !!error,
           validationErrorMessages: getValidationMessages(error),
         });
