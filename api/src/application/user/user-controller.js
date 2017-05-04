f4df377cda5a547cd426ea75fb80488b0a6a6e6d
Michael Sinnes <michael.sinnes@gmail.com>
2017-05-04 13:38:17 -0700

    Defect/oss98 password issues (#44)
    * will filter the cancel button on the set password page
    
    * changed to boolean
    
    * checking redirect instead of title
    
    * forced eval to falsy
    
    * debugging the returnTo statement
    
    * forced the check on redirect to a boolean
    
    * removed the string check on undefined

--- api/src/application/user/user-controller.js
+++ api/src/application/user/user-controller.js
@@ -108,9 +108,10 @@ module.exports = (
   const getPasswordResetHandler = (method, title) => {
     if (method === 'GET') {
       return (request, reply, source, error) => {
+        const redirectSet = request.query.redirect_uri == undefined;
         reply.view('reset-password', {
           title: title,
-          returnTo: `${request.query.redirect_uri}?status=cancelled`,
+          returnTo: (redirectSet) ? false : `${request.query.redirect_uri}?status=cancelled`,
           error: !!error,
           validationErrorMessages: getValidationMessages(error),
         });
