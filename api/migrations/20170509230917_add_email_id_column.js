8719a521aadc0a66cc26785f67784a2db407221c
Michael Sinnes <michael.sinnes@gmail.com>
2017-05-15 16:51:19 -0700

    refactored migration to work for mysql and postgres

--- api/migrations/20170509230917_add_email_id_column.js
+++ api/migrations/20170509230917_add_email_id_column.js
@@ -1,18 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  if (process.env.OIDC_DB_ADAPTER === 'mysql') {
-    return knex.raw(`
-      UPDATE SIP_user SET email_id=LCASE(email);
-    `);
-  }
+    return knex.schema.table('SIP_user', table => {
+      table.string('email_id').notNullable().unique();
+    }).then(knex.select().from('SIP_user').then(users => {
+      if (process.env.OIDC_DB_ADAPTER === 'mysql') {
+        return knex.raw(`
+          UPDATE SIP_user SET email_id=LCASE(email);
+        `);
+      } else {
+        users.forEach(user => {
+          const emailID = user.email.toLowerCase();
+          knex('SIP_user').update('email_id', emailID).where('id', user.id);
+        });
 
-  return knex.schema.table('SIP_user', table => {
-    table.string('email_id').notNullable().unique();
-  }).then(knex.select().from('SIP_user').then(users => {
-    users.forEach(user => {
-      const emailID = user.email.toLowerCase();
-      knex('SIP_user').update('email_id', emailID).where('id', user.id);
-    });
+      }
   }));
 };
 
