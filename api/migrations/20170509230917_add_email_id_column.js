ecd249fb69a99348f8886a6703f0d363fa769948
Michael Sinnes <michael.sinnes@gmail.com>
2017-06-02 16:54:38 -0700

    removed nullable constraint

--- api/migrations/20170509230917_add_email_id_column.js
+++ api/migrations/20170509230917_add_email_id_column.js
@@ -1,20 +1,21 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.table('SIP_user', table => {
-      table.string('email_id').notNullable().unique();
-    }).then(knex.select().from('SIP_user').then(users => {
+      table.string('email_id').unique();
+    }).then(() => {
       if (process.env.OIDC_DB_ADAPTER === 'mysql') {
         return knex.raw(`
           UPDATE SIP_user SET email_id=LCASE(email);
         `);
       } else {
-        users.forEach(user => {
-          const emailID = user.email.toLowerCase();
-          knex('SIP_user').update('email_id', emailID).where('id', user.id);
+        knex.select().from('SIP_user').then(users => {
+          users.forEach(user => {
+            const emailID = user.email.toLowerCase();
+            knex('SIP_user').update('email_id', emailID).where('id', user.id);
+          });
         });
-
       }
-  }));
+  });
 };
 
 exports.down = function(knex, Promise) {
