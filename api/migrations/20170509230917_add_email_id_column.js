36c3cc8c10be0f56f48a4b89d75c07aadf4710a5
Michael Sinnes <michael.sinnes@gmail.com>
2017-06-08 16:28:43 -0700

    set migration to add email_id column

--- api/migrations/20170509230917_add_email_id_column.js
+++ api/migrations/20170509230917_add_email_id_column.js
@@ -1,21 +1,16 @@
 
 exports.up = function(knex, Promise) {
+
     return knex.schema.table('SIP_user', table => {
-      table.string('email_id').unique();
-    }).then(() => {
-      if (process.env.OIDC_DB_ADAPTER === 'mysql') {
-        return knex.raw(`
-          UPDATE SIP_user SET email_id=LCASE(email);
-        `);
-      } else {
-        knex.select().from('SIP_user').then(users => {
-          users.forEach(user => {
-            const emailID = user.email.toLowerCase();
-            knex('SIP_user').update('email_id', emailID).where('id', user.id);
-          });
+        table.string('email_id').notNull().defaultTo('');
+      }).then(() => {
+        return knex.select().table('SIP_user').then(userEmails => {
+          return Promise.all(
+            userEmails.map((user) => {
+              return knex('SIP_user').update({ email_id: user.email.toLowerCase() }).where('id', user.id);
+            }));
         });
-      }
-  });
+    });
 };
 
 exports.down = function(knex, Promise) {
