0831462f184716f5f386049fa785d2d8655cb3a3
Michael Sinnes <michael.sinnes@gmail.com>
2017-06-09 08:50:00 -0700

    removed default value from field

--- api/migrations/20170509230917_add_email_id_column.js
+++ api/migrations/20170509230917_add_email_id_column.js
@@ -1,15 +1,7 @@
 
 exports.up = function(knex, Promise) {
-
     return knex.schema.table('SIP_user', table => {
-        return table.string('email_id').notNull().defaultTo('');
-      }).then(() => {
-        return knex.select().table('SIP_user').then(userEmails => {
-          return Promise.all(
-            userEmails.map((user) => {
-              return knex('SIP_user').update({ email_id: user.email.toLowerCase() }).where('id', user.id);
-            }));
-        });
+        return table.string('email_id').notNull();
     });
 };
 
