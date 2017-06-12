3e18146ce3ca97ff31602116cee0a6c408175bdf
Michael Sinnes <michael.sinnes@gmail.com>
2017-06-12 10:37:09 -0700

    renamed file

---
+++ api/migrations/20170509230917_add_email_lower_column.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('SIP_user', table => {
+        return table.string('email_lower').unique();
+    }).then(() => {
+      return knex.select().from('SIP_user').then(users => {
+        return Promise.all(users.map(user => {
+          return knex('SIP_user').where('id', user.id).update({ email_id: user.email.toLowerCase() });
+        }));
+      });
+    }).then(() => {
+      return knex.schema.alterTable('SIP_user', (table) => {
+        table.string('email_lower').notNullable().alter();
+      });
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('SIP_user', table => {
+    table.dropColumn('email_lower');
+  });
+};
