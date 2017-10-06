54b3d949a0b77a3d5ce9b94f904711a344070986
spruce-bruce <aaron@synapsestudios.com>
2017-10-06 12:25:09 -0700

    throw view object construction into a file

--- api/src/plugins/openid-connect/addRoutes.js
+++ api/src/plugins/openid-connect/addRoutes.js
@@ -1,38 +1,19 @@
 const querystring = require('querystring');
+const views = require('./views');
 
 module.exports = (server, issuer, options) => {
   server.route({
     method: 'GET',
     path: '/interaction/{grant}',
     handler: async (request, reply) => {
-      const cookie = await server.plugins['open-id-connect'].provider.interactionDetails(request.raw.req);
-      const client = server.plugins['open-id-connect'].provider.Client.find(cookie.params.client_id);
+      const provider = server.plugins['open-id-connect'].provider;
+      const cookie = await provider.interactionDetails(request.raw.req);
+      const client = provider.Client.find(cookie.params.client_id);
 
       if (cookie.interaction.error === 'login_required') {
-        reply.view('login', {
-          client,
-          cookie,
-          title: 'Log In',
-          debug: querystring.stringify(cookie.params, ',<br/>', ' = ', {
-            encodeURIComponent: value => value,
-          }),
-          interaction: querystring.stringify(cookie.interaction, ',<br/>', ' = ', {
-            encodeURIComponent: value => value,
-          }),
-          forgotPasswordPath: `${issuer}/user/forgot-password?client_id=${cookie.params.client_id}&response_type=${cookie.params.response_type}&scope=${cookie.params.scope}&redirect_uri=${cookie.params.redirect_uri}&nonce=${cookie.params.nonce}`,
-        });
+        reply.view('login', views.login(cookie, client));
       } else {
-        reply.view('interaction', {
-          client,
-          cookie,
-          title: 'Authorize',
-          debug: querystring.stringify(cookie.params, ',<br />', ' = ', {
-            encodeURIComponent: value => value,
-          }),
-          interaction: querystring.stringify(cookie.interaction, ',<br/>', ' = ', {
-            encodeURIComponent: value => value,
-          }),
-        });
+        reply.view('interaction', views.interaction(cookie, client));
       }
     },
     config: {
@@ -59,38 +40,26 @@ module.exports = (server, issuer, options) => {
   server.route({
     method: 'POST',
     path: '/interaction/{grant}/login',
-    handler: (request, reply) => {
-      options.authenticateUser(request.payload.login, request.payload.password)
-        .then(account => {
-          if (account) {
-            const result = {
-              login: {
-                account: account.accountId,
-                acr: 'urn:mace:incommon:iap:bronze',
-                amr: ['pwd'],
-                remember: request.payload.remember,
-                ts: Math.floor(Date.now() / 1000),
-              },
-              consent: {}
-            };
-            server.plugins['open-id-connect'].provider.interactionFinished(request.raw.req, request.raw.res, result);
-          } else {
-            const cookie = server.plugins['open-id-connect'].provider.interactionDetails(request.raw.req);
-            const client = server.plugins['open-id-connect'].provider.Client.find(cookie.params.client_id);
-            reply.view('login', {
-              error: 'Invalid email password combination',
-              client,
-              cookie,
-              title: 'Log In',
-              debug: querystring.stringify(cookie.params, ',<br/>', ' = ', {
-                encodeURIComponent: value => value,
-              }),
-              interaction: querystring.stringify(cookie.interaction, ',<br/>', ' = ', {
-                encodeURIComponent: value => value,
-              }),
-            });
-          }
-        });
+    handler: async (request, reply) => {
+      const account = await options.authenticateUser(request.payload.login, request.payload.password);
+      const provider = server.plugins['open-id-connect'].provider;
+      if (account) {
+        const result = {
+          login: {
+            account: account.accountId,
+            acr: 'urn:mace:incommon:iap:bronze',
+            amr: ['pwd'],
+            remember: request.payload.remember,
+            ts: Math.floor(Date.now() / 1000),
+          },
+          consent: {}
+        };
+        provider.interactionFinished(request.raw.req, request.raw.res, result);
+      } else {
+        const cookie = await provider.interactionDetails(request.raw.req);
+        const client = provider.Client.find(cookie.params.client_id);
+        reply.view('login', views.login(cookie, client, 'Invalid email password combination'));
+      }
     },
     config: {
       state: {
