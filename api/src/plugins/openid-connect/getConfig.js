af5eb0b7c0a3c6cecec7128ebe0c4fa218f49664
spruce-bruce <aaron@synapsestudios.com>
2017-10-09 17:59:47 -0700

    declare admin scope

--- api/src/plugins/openid-connect/getConfig.js
+++ api/src/plugins/openid-connect/getConfig.js
@@ -60,5 +60,6 @@ module.exports = options => {
     subjectTypes: ['public', 'pairwise'],
     pairwiseSalt: 'da1c442b365b563dfc121f285a11eedee5bbff7110d55c88',
     interactionUrl: async (ctx, interaction) => `/interaction/${ctx.oidc.uuid}`,
+    scopes: ['admin'],
   }
 }
