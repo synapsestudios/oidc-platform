cd53929422b193d100569156d7972a34171004a1
spruce-bruce <aaron@synapsestudios.com>
2017-10-13 15:09:07 -0700

    cleanup for no reason

--- api/migrations/20171013195728_drop_template_id_field_in_client.js
+++ api/migrations/20171013195728_drop_template_id_field_in_client.js
@@ -1,9 +1,7 @@
 
-exports.up = knex => {
-  return knex.schema.table('SIP_client', t => {
-    t.dropForeign('reset_password_template_id');
-    t.dropColumn('reset_password_template_id');
-  });
-};
+exports.up = knex => knex.schema.table('SIP_client', t => {
+  t.dropForeign('reset_password_template_id');
+  t.dropColumn('reset_password_template_id');
+});
 
 exports.down = () => {/* noop */};
