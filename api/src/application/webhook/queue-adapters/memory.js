f61c19cd13f96be5761585b392daec0a5574f691
spruce-bruce <aaron@synapsestudios.com>
2017-10-30 15:28:32 -0700

    implement job processing with wreck

--- api/src/application/webhook/queue-adapters/memory.js
+++ api/src/application/webhook/queue-adapters/memory.js
@@ -1,10 +1,12 @@
 const uuid = require('uuid');
+const Wreck = require('wreck');
+
 const maxRetries = 2;    // make configurable
 const retryDelay = 1000; // make configurable
 
 const q = require('queue')({
   concurrency: 2, // make configurable
-  timeout: 200,   // make configurable
+  timeout: 2000,   // make configurable
   autostart: true,
 });
 
@@ -23,16 +25,28 @@ const attemptRetry = job => {
 getJob = data => {
   let job = cb => {
     console.log(`processing job! ${data.url}:${data.payload.webhook_id}`);
+    const timestamp = new Date().getTime()/1000|0;
+
+    Wreck.post(data.url, { payload: data.payload }, (err, response, payload) => {
+      job.attempts.push({
+        timestamp,
+        response,
+        error: err,
+      });
+
+      cb(err);
+    });
   }
 
   job.id = uuid.v4();
   job.retries = 0;
+  job.attempts = [];
   job.data = data;
   return job;
 }
 
 q.on('error', (err, job) => {
-  console.log('we have an error!');
+  console.log(`we have an error! ${err.message}`);
   attemptRetry(job);
 });
 
--- test-client/test-server/index.js
+++ test-client/test-server/index.js
@@ -1,12 +1,12 @@
 const Hapi = require('hapi');
 const Joi = require('joi');
+const Boom = require('boom');
 
 const inviteHandler = require('./inviteHandler');
-const postEmailTemplateHandler = require('./postEmailTemplateHandler');
 
 const server = new Hapi.Server();
 
-server.connection({ port: 8080, host: 'localhost' });
+server.connection({ port: 8080 });
 
 server.route({
   method: 'POST',
@@ -24,9 +24,26 @@ server.route({
 });
 
 server.route({
-  method: 'GET',
-  path: '/test-email-template-post',
-  handler: postEmailTemplateHandler,
+  method: 'POST',
+  path: '/webhook',
+  handler: (request, reply) => {
+    const random = Math.floor(Math.random() * 100);
+    switch (true) {
+      case (random < 5):
+        console.log('responding with 5xx');
+        reply(Boom.badImplementation());
+        break;
+      case (random >= 5 && random < 10):
+        console.log('responding with 4xx');
+        reply(Boom.forbidden());
+        break;
+      default:
+        console.log('responding with 1 second delay');
+        setTimeout(() => {
+          reply(request.payload);
+        }, 1000);
+    }
+  },
 });
 
 server.start((err) => {
