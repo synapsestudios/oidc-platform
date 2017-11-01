02c457908865562e6f4bc6312c8687d35758272d
spruce-bruce <aaron@synapsestudios.com>
2017-11-01 14:04:49 -0700

    delete abandoned file

--- api/src/application/webhook/webhook-queue.js
+++
@@ -1,7 +0,0 @@
-const adapters = require('./queue-adapters');
-const webhooksConfig = require('../../../config')('/webhooks');
-
-// validate the adapter
-// figure out what to do when webhooks are turned off
-
-module.exports = adapters[webhooksConfig.adapter];
