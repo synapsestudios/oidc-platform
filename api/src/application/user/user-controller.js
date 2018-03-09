5272b3081d1cfdd6ef01d191c0ff7e4b1068f3c3
David <david@synapsestudios.com>
2018-03-09 11:48:32 -0700

    Add code to destroy email-token when canceling email change

--- api/src/application/user/user-controller.js
+++ api/src/application/user/user-controller.js
@@ -53,9 +53,12 @@ module.exports = (
           await userEmails.sendChangeEmailVerifyEmail(user, client, email, request.query);
           break;
         case 'cancel_new':
+          const emailToken = bookshelf.model('email_token').where({user_id:user.get('id')})
           user.set('pending_email', null);
           user.set('pending_email_lower', null);
+
           await user.save();
+          await emailToken.destroy();
           break;
         case 'change':
           const isAuthenticated = await comparePasswords(current, user);
