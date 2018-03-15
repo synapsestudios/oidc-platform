df85b1b3c466064fe431da820e1277c63b81b6b0
David <david@synapsestudios.com>
2018-03-15 16:44:45 -0700

    Add rollbarLogger to logger

--- api/src/lib/logger.js
+++ api/src/lib/logger.js
@@ -1,7 +1,23 @@
 const winston = require('winston');
 const util = require('util');
-const Raven = require('raven');
 const transports = [new winston.transports.Console()];
+
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar('ff3ef8cca74244eabffb17dc2365e7bb');
+
+const rollbarLogger = winston.transports.CustomLogger = function () {
+  this.name = 'rollbarLogger';
+  this.level = 'error';
+};
+util.inherits(rollbarLogger, winston.Transport);
+rollbarLogger.prototype.log = function (level, msg, meta, callback) {
+  rollbar.error(meta.message, meta);
+  callback(null, true);
+};
+
+
+
+const Raven = require('raven');
 const sentryLogger = winston.transports.CustomLogger = function () {
   this.name = 'sentryLogger';
   this.level = 'error';
@@ -13,6 +29,7 @@ sentryLogger.prototype.log = function (level, msg, meta, callback) {
 };
 
 transports.push(new sentryLogger());
+transports.push(new rollbarLogger());
 module.exports = new winston.Logger({
   transports: transports,
   filters: [
