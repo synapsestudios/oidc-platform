8ef86e36138ed33bc7196b35bfb16625b2a92bd5
David <david@synapsestudios.com>
2018-03-15 16:21:13 -0700

    Add sentryLogger to logger

--- api/src/lib/logger.js
+++ api/src/lib/logger.js
@@ -1,8 +1,20 @@
 const winston = require('winston');
+const util = require('util');
+const Raven = require('raven');
+const transports = [new winston.transports.Console()];
+const sentryLogger = winston.transports.CustomLogger = function () {
+  this.name = 'sentryLogger';
+  this.level = 'error';
+};
+util.inherits(sentryLogger, winston.Transport);
+sentryLogger.prototype.log = function (level, msg, meta, callback) {
+  Raven.captureMessage(msg);
+  callback(null, true);
+};
+
+transports.push(new sentryLogger());
 module.exports = new winston.Logger({
-  transports: [
-    new winston.transports.Console(),
-  ],
+  transports: transports,
   filters: [
     (level, msg, meta) => msg.trim(),  // shouldn't be necessary, but good-winston is adding \n
   ],
