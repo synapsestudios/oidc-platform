ac12d8de05ba559317aad849ed49c3740301b58a
spruce-bruce <aaron@synapsestudios.com>
2018-07-11 14:37:45 -0700

    update oidc-session-scheme to recover session
    id_token_hint is provided and cookies are not set
    we can attempt to recover the session and reset
    the user's cookies. We assume in this case that
    the cookies were lost erroneously (as sometimes
    happens in our mobile environments)

--- api/src/plugins/oidc-session-scheme.js
+++ api/src/plugins/oidc-session-scheme.js
@@ -1,20 +1,61 @@
 const Boom = require('boom');
+const base64url = require('base64url');
+const uuid = require('uuid/v4');
+
+const epochTime = (date = Date.now()) => Math.floor(date / 1000);
+
+async function getSession(req, res) {
+  const ctx = this.app.createContext(req, res);
+  return this.Session.get(ctx);
+}
+
+async function setProviderSession(req, res, {
+  account,
+  ts = epochTime(),
+  remember = true,
+  clients = [],
+} = {}) {
+  const session = await getSession.call(this, req, res);
+  Object.assign(session, {
+    account,
+    loginTs: ts,
+  });
+  if (!remember) session.transient = true;
+  clients.forEach((clientId) => {
+    session.sidFor(clientId, uuid());
+  });
+  await session.save();
+  return session;
+}
 
 exports.register = function (server, pluginOptions, next) {
   server.auth.scheme('oidc_session', () => {
     return {
       async authenticate(request, reply) {
-        // only do this when cookies aren't present and some token hint is provided or whatever
-        // await server.plugins['open-id-connect'].provider.setProviderSession(request.raw.req, request.raw.res, {
-        //   account: '5b5b85ab-dfce-4d9b-b34e-7d8ff34cf35c'
-        // });
-
-        // const koaCtx = server.plugins['open-id-connect'].provider.app.createContext(request.raw.req, request.raw.res);
-        // const x = await request.server.plugins['open-id-connect'].provider.Session.find(koaCtx.cookies.get('_session'));
-        // console.log(x);
 
         if (!request.state._session) {
-          reply(Boom.forbidden());
+          if (request.query.id_token_hint) {
+            // only do this when cookies aren't present and some token hint is provided or whatever
+            const parts = String(request.query.id_token_hint).split('.');
+            const token = {
+              header: JSON.parse(base64url.decode(parts[0])),
+              payload: JSON.parse(base64url.decode(parts[1])),
+            };
+
+            try {
+              const session = await setProviderSession.call(server.plugins['open-id-connect'].provider, request.raw.req, request.raw.res, {
+                account: token.payload.sub
+              });
+
+              reply.continue({
+                credentials: session,
+              });
+            } catch(e) {
+              reply(e);
+            }
+          } else {
+            reply(Boom.forbidden());
+          }
         } else {
           try {
             var session = await request.server.plugins['open-id-connect'].provider.Session.find(request.state._session);
