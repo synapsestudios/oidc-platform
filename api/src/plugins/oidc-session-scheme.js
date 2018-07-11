c46a349c79f345f8298a24d4e524ea8fde1f9abe
spruce-bruce <aaron@synapsestudios.com>
2018-07-11 09:00:12 -0700

    commented out session experiment stuff

--- api/src/plugins/oidc-session-scheme.js
+++ api/src/plugins/oidc-session-scheme.js
@@ -1,10 +1,18 @@
 const Boom = require('boom');
 
 exports.register = function (server, pluginOptions, next) {
-  server.auth.scheme('oidc_session', (server, schemeOptions) => {
+  server.auth.scheme('oidc_session', () => {
     return {
-
       async authenticate(request, reply) {
+        // only do this when cookies aren't present and some token hint is provided or whatever
+        // await server.plugins['open-id-connect'].provider.setProviderSession(request.raw.req, request.raw.res, {
+        //   account: '5b5b85ab-dfce-4d9b-b34e-7d8ff34cf35c'
+        // });
+
+        // const koaCtx = server.plugins['open-id-connect'].provider.app.createContext(request.raw.req, request.raw.res);
+        // const x = await request.server.plugins['open-id-connect'].provider.Session.find(koaCtx.cookies.get('_session'));
+        // console.log(x);
+
         if (!request.state._session) {
           reply(Boom.forbidden());
         } else {
