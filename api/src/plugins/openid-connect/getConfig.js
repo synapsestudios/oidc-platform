12eac6316ab662a4daa1a513fe7df84f862f6fff
spruce-bruce <aaron@synapsestudios.com>
2019-01-31 13:18:30 -0800

    use the new pairwiseSaltIdentifier function

--- api/src/plugins/openid-connect/getConfig.js
+++ api/src/plugins/openid-connect/getConfig.js
@@ -70,7 +70,7 @@ module.exports = options => {
       }
     },
     subjectTypes: ['public', 'pairwise'],
-    pairwiseSalt: options.pairwiseSalt,
+    pairwiseSaltIdentifier: async () => options.pariwiseSalt,
     interactionUrl: async (ctx, interaction) => `/interaction/${ctx.oidc.uuid}`,
     scopes: ['admin', 'superadmin'],
   }
