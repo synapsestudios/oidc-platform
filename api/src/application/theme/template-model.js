5d335decfedfd377c35fdf53358ddbf205ff7249
spruce-bruce <aaron@synapsestudios.com>
2019-02-15 16:38:15 -0800

    get rid of commented out old stuff

--- api/src/application/theme/template-model.js
+++ api/src/application/theme/template-model.js
@@ -3,28 +3,6 @@ const fs = require('fs');
 const { promisify } = require('util');
 const defaultLayouts = require('./defaultLayouts');
 
-// const defaultLayouts = {
-//   'end_session': 'default.hbs',
-//   'forgot-password-success': 'default.hbs',
-//   'forgot-password': 'default.hbs',
-//   'interaction': 'default.hbs',
-//   'login': 'default.hbs',
-//   'reset-password-success': 'default.hbs',
-//   'reset-password': 'default.hbs',
-//   'user-profile': 'default.hbs',
-//   'user-registration': 'default.hbs',
-//   'change-password': 'default.hbs',
-//   'email-settings': 'default.hbs',
-//   'email-verify-success': 'default.hbs',
-
-//   'forgot-password-email': 'email.hbs',
-//   'invite-email': 'email.hbs',
-//   'change-password-success-email': 'email.hbs',
-//   'email-verify-email': 'email.hbs',
-//   'change-email-verify-email': 'email.hbs',
-//   'change-email-alert-email': 'email.hbs',
-// };
-
 module.exports = () => ({
   tableName: 'SIP_template',
   idAttribute: false,
