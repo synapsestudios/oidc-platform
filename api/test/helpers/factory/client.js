7f7652b0f664cfbffd6adf1866fb15b7a84d0488
Zachary Chavez <zpchavez@gmail.com>
2020-04-20 16:48:30 -0700

    Add webhook POST tests

---
+++ api/test/helpers/factory/client.js
@@ -0,0 +1,15 @@
+const bookshelf = require('../../../src/lib/bookshelf');
+const Client = bookshelf.model('client');
+
+const initialize = factory => {
+  factory.define('client', Client,  {
+    client_id: factory.chance('guid', { version: 4 }),
+    client_secret: factory.chance('guid', { version: 4 }),
+    client_name: factory.chance('word'),
+    client_uri: 'https://sso-client.test',
+    application_type: 'web',
+    id_token_signed_response_alg: 'RS256',
+  });
+};
+
+module.exports = initialize;
--- api/test/helpers/factory/index.js
+++ api/test/helpers/factory/index.js
@@ -1,11 +1,13 @@
 const FactoryGirl = require('factory-girl');
 
 const initializeUserFactory = require('./user');
+const initializeClientFactory = require('./client');
 
 const factory = FactoryGirl.factory;
 const adapter = new FactoryGirl.BookshelfAdapter();
 factory.setAdapter(adapter);
 
 initializeUserFactory(factory);
+initializeClientFactory(factory);
 
 module.exports = factory;
