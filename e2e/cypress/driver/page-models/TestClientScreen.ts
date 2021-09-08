38814fc6e453a481aa079f5619e0f1839e412018
Andrew Reida <andrew@synapsestudios.com>
2021-09-08 15:10:50 -0700

    Implementing basic e2e test

---
+++ e2e/cypress/driver/page-models/TestClientScreen.ts
@@ -0,0 +1,9 @@
+export class TestClientScreen {
+  static visit() {
+    cy.visit(Cypress.env('frontend_base_url'));
+  }
+
+  static clickLoginLink() {
+    cy.get('a').contains('Log In').click();
+  }
+}
