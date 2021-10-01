94f6235b8444d6de12a3027e58643a2957f0c80a
Alex <alex@synapsestudios.com>
2021-10-01 13:41:52 -0700

    Add tests for POST /user/forgot-password, fix status code

---
+++ api/test/application/user/forgot-password-post.js
@@ -0,0 +1,67 @@
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { stringify } = require('querystring');
+const sinon = require('sinon');
+const uuid = require('uuid');
+
+const getServer = require('../../server');
+const { truncateAll } = require('../../helpers/db');
+const factory = require('../../helpers/factory');
+
+const { describe, it, before, beforeEach, after, afterEach } = (exports.lab =
+  Lab.script());
+
+describe('POST /user/forgot-password', () => {
+  let server, client;
+
+  before(async () => {
+    server = await getServer();
+  });
+
+  beforeEach(async () => {
+    client = await factory.create('client');
+    await client.refresh({ withRelated: 'redirect_uris' });
+  });
+
+  after(async () => {
+    await truncateAll();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('returns 400 when the payload is missing', async () => {
+    const query = {
+      client_id: client.get('client_id'),
+      redirect_uri: client.related('redirect_uris').at(0).get('uri'),
+      code_challenge: uuid.v4(),
+    };
+    const res = await server.inject({
+      method: 'POST',
+      url: `/user/forgot-password?${stringify(query)}`,
+    });
+
+    expect(res.statusCode).to.equal(400);
+  });
+
+  it('includes code_challenge in the password recovery form action', async () => {
+    const query = {
+      client_id: client.get('client_id'),
+      redirect_uri: client.related('redirect_uris').at(0).get('uri'),
+      code_challenge: uuid.v4(),
+    };
+    const res = await server.inject({
+      method: 'POST',
+      url: `/user/forgot-password?${stringify(query)}`,
+      payload: {
+        email: 'fakeuser@example.com',
+      },
+    });
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.result).to.match(
+      new RegExp(`href="/op/auth?[^"]+${query.code_challenge}`)
+    );
+  });
+});
---
+++ api/test/application/user/reset-password-post.js
@@ -0,0 +1,107 @@
+const Lab = require('@hapi/lab');
+const { expect } = require('@hapi/code');
+const { stringify } = require('querystring');
+const sinon = require('sinon');
+const uuid = require('uuid');
+
+const getServer = require('../../server');
+const { truncateAll } = require('../../helpers/db');
+const factory = require('../../helpers/factory');
+
+const { describe, it, before, beforeEach, after, afterEach } = (exports.lab =
+  Lab.script());
+
+describe('POST /user/reset-password', () => {
+  let server, client;
+
+  before(async () => {
+    server = await getServer();
+  });
+
+  beforeEach(async () => {
+    client = await factory.create('client');
+    await client.refresh({ withRelated: 'redirect_uris' });
+  });
+
+  after(async () => {
+    await truncateAll();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('returns 403 forbidden if token is not supplied', async () => {
+    const res = await server.inject({
+      method: 'POST',
+      url: '/user/reset-password',
+    });
+
+    expect(res.statusCode).to.equal(403);
+  });
+
+  it('returns 400 when the payload is missing', async () => {
+    const user = await factory.create('user');
+    const token = await factory.create('emailToken', { user_id: user.id });
+
+    const query = {
+      token: token.get('token'),
+      client_id: client.get('client_id'),
+      redirect_uri: client.related('redirect_uris').at(0).get('uri'),
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `/user/reset-password?${stringify(query)}`,
+    });
+
+    expect(res.statusCode).to.equal(400);
+  });
+
+  it('returns 400 when passwords differ', async () => {
+    const user = await factory.create('user');
+    const token = await factory.create('emailToken', { user_id: user.id });
+
+    const query = {
+      token: token.get('token'),
+      client_id: client.get('client_id'),
+      redirect_uri: client.related('redirect_uris').at(0).get('uri'),
+    };
+
+    const res = await server.inject({
+      method: 'POST',
+      url: `/user/reset-password?${stringify(query)}`,
+      payload: {
+        password: 'abcdefgh',
+        pass2: 'ijklmnop',
+      },
+    });
+
+    expect(res.statusCode).to.equal(400);
+  });
+
+  it('includes code_challenge in the login link URL', async () => {
+    const user = await factory.create('user');
+    const token = await factory.create('emailToken', { user_id: user.id });
+
+    const query = {
+      token: token.get('token'),
+      client_id: client.get('client_id'),
+      redirect_uri: client.related('redirect_uris').at(0).get('uri'),
+      code_challenge: uuid.v4(),
+    };
+    const res = await server.inject({
+      method: 'POST',
+      url: `/user/reset-password?${stringify(query)}`,
+      payload: {
+        password: 'newpassword',
+        pass2: 'newpassword',
+      },
+    });
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.result).to.match(
+      new RegExp(`href="/op/auth?[^"]+${query.code_challenge}`)
+    );
+  });
+});
