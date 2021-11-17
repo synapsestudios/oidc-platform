cee0f46446195352ef7ca7dc24a2492176c83bf5
Bryce Egley <bryce.egley@synapsestudios.com>
2021-11-17 10:03:08 -0700

    Tests for email whitelist added.  Changed check-whitelist return object and removed some redundant validation (#524)

---
+++ api/test/application/email/check-whitelist.js
@@ -0,0 +1,72 @@
+const Lab = require('@hapi/lab');
+const Code = require('@hapi/code');
+const { truncateAll } = require('../../helpers/db');
+
+const { describe, it, after, beforeEach, afterEach } = exports.lab = Lab.script();
+const sinon = require('sinon');
+const { expect } = Code;
+const { stubConfig } = require("../../helpers/stubs");
+const checkWhitelist = require('../../../src/application/email/check-whitelist');
+
+
+describe('white list', () => {
+
+    let confidenceGetStub, confidenceLoadStub;
+
+    function fakeReject(message) {
+        return new Promise((resolve, reject) => reject(message));
+    }
+
+    beforeEach(async () => {  
+      confidenceGetStub = sinon.stub();
+      confidenceLoadStub = sinon.stub();
+      stubConfig(confidenceGetStub, confidenceLoadStub);
+
+    });
+
+    after(async () => {
+      await truncateAll();
+    });
+
+    afterEach(async() => {
+      sinon.restore();
+    });
+
+    it(`rejects with error if whitelist is set with no trap`, async () => {
+        confidenceGetStub.withArgs('email/trap').returns('');
+        confidenceGetStub.withArgs('email/whitelist').returns('valid.com');
+
+        await expect(checkWhitelist('myemail@valid.com', fakeReject)).to.reject(Error, 'trap option must be set if using whitelist');
+    });
+
+    it(`rejects with error if trap is set with no whitelist`, async () => {
+        confidenceGetStub.withArgs('email/trap').returns('admin@reject-domain.com');
+        confidenceGetStub.withArgs('email/whitelist').returns('');
+
+        await expect(checkWhitelist('myemail@valid.com', fakeReject)).to.reject(Error, 'whitelist option must be set if using email trap');
+    });
+
+    it(`returns the email address if no whitelist or trap is set`, async () => {
+        confidenceGetStub.withArgs('email/trap').returns('');
+        confidenceGetStub.withArgs('email/whitelist').returns('');
+
+        const response = checkWhitelist('myemail@valid.com', fakeReject);
+        expect(response).to.equal('myemail@valid.com');
+    });
+
+    it(`returns the email address if domain is in the whitelist`, async () => {
+        confidenceGetStub.withArgs('email/trap').returns('admin@reject-domain.com');
+        confidenceGetStub.withArgs('email/whitelist').returns('valid.com');
+
+        const response = checkWhitelist('myemail@valid.com', fakeReject);
+        expect(response).to.equal('myemail@valid.com');
+    });
+
+    it(`returns the email routed to the trap if domain is not in the whitelist`, async () => {
+        confidenceGetStub.withArgs('email/trap').returns('admin@reject-domain.com');
+        confidenceGetStub.withArgs('email/whitelist').returns('valid.com');
+
+        const response = checkWhitelist('myemail@invalid.com', fakeReject);
+        expect(response).to.equal('admin+myemailinvalidcom@reject-domain.com');
+    });
+})   
