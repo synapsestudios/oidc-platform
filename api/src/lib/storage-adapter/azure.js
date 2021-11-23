d4e1b48657cbf9d621ec7572197cd054d0695aed
Bryce Egley <bryce.egley@synapsestudios.com>
2021-11-23 13:57:58 -0700

    streamToString function now in it's own lib module with testing

--- api/src/lib/storage-adapter/azure.js
+++ api/src/lib/storage-adapter/azure.js
@@ -1,20 +1,6 @@
 const config = require('../../../config');
 const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
-
-  // [Node.js only] A helper method used to read a Node.js readable stream into string
-  async function streamToString(readableStream) {
-    return new Promise((resolve, reject) => {
-      const chunks = [];
-      readableStream.on("data", (data) => {
-        chunks.push(data.toString());
-      });
-      readableStream.on("end", () => {
-        resolve(chunks.join(""));
-      });
-      readableStream.on("error", reject);
-    });
-  }
-  
+const streamToString = require('../stream-to-string');
   module.exports = function() {
 
   const azureStorageAccount = config('/azure/storageAccount');
---
+++ api/src/lib/stream-to-string.js
@@ -0,0 +1,13 @@
+// [Node.js only] A helper method used to read a Node.js readable stream into string
+module.exports = async function(readableStream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on("data", (data) => {
+            chunks.push(data.toString());
+        });
+        readableStream.on("end", () => {
+            resolve(chunks.join(""));
+        });
+        readableStream.on("error", reject);
+    });
+}
