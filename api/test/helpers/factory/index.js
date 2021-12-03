fdeaea8011c1bb4c01ea7a7d3250eef54a4fda85
Alexander Peters <alex@synapsestudios.com>
2021-12-03 08:56:12 -0800

    Test/user profile routes (#534)
    * GET and POST to user profile.  Token helper added.
    
    * Check for redirect in GET/POST /user/profile, remove 404s
    
    * Add supertest, authenticate users for happy-path tests
    
    Co-authored-by: Bryce Egley <bryce.egley@synapsestudios.com>

--- api/test/helpers/factory/index.js
+++ api/test/helpers/factory/index.js
@@ -3,6 +3,8 @@ const FactoryGirl = require('factory-girl');
 const initializeUserFactory = require('./user');
 const initializeClientFactory = require('./client');
 const initializeClientRedirectUriFactory = require('./clientRedirectUri');
+const initializeClientResponseTypeFactory = require('./clientResponseType');
+const initializeClientGrantFactory = require('./clientGrant');
 const initializeEmailTokenFactory = require('./emailToken');
 
 const factory = FactoryGirl.factory;
@@ -12,6 +14,8 @@ factory.setAdapter(adapter);
 initializeUserFactory(factory);
 initializeClientFactory(factory);
 initializeClientRedirectUriFactory(factory);
+initializeClientResponseTypeFactory(factory);
+initializeClientGrantFactory(factory);
 initializeEmailTokenFactory(factory);
 
 module.exports = factory;
